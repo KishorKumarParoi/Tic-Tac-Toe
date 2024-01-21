716b34461fd9d9437ee0bd62afab0ee992c4db42
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2024-01-21 21:36:12 +0600

    first version of Tic-Tac-Toe game is working and it showing who is the Winner

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,32 @@ import Square from "./components/Square";
 
 export default function App() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
   function handleClick(index) {
     const nextSquares = [...squares];
-    nextSquares[index] = 'X';
+    if (squares[index] || calculateWinner(squares)) return;
+
+    if (xIsNext) {
+      nextSquares[index] = 'X';
+    } else {
+      nextSquares[index] = 'O';
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+  }
+
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board">
         <div className="board-row">
           <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
@@ -29,4 +48,25 @@ export default function App() {
       </div>
     </>
   )
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
 }
