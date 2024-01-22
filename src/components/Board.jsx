239d2c8e086f28be2202885f5d168eae00ceae0b
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2024-01-23 00:36:47 +0600

    changing Square on Clicking by lifting up state

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,45 +1,36 @@
-import CalculateWinner from "./CalculateWinner.jsx";
-import Square from "./Square.jsx";
+import { useState } from "react";
 
-export default function Board({ xIsNext, squares, onPlay }) {
+function Square({ value, onSquareClick }) {
 
-    function handleClick(index) {
+    return (
+        <>
+            <button className="border border-5 border-gray-500  bg-white text-Black text-7xl h-32 w-32 font-semibold leading-9 m-2" onClick={onSquareClick}>{value}</button>
+        </>
+    )
+}
+
+export default function Board() {
+    const [squares, setSquares] = useState(Array(9).fill(null));
+    function handleClick(i) {
         const nextSquares = squares.slice();
-        if (squares[index] || CalculateWinner(squares)) return;
-
-        if (xIsNext) {
-            nextSquares[index] = 'X';
-        } else {
-            nextSquares[index] = 'O';
-        }
-        onPlay(nextSquares);
-    }
-
-    const winner = CalculateWinner(squares);
-    const [a, b, c] = winner || [];
-    let status;
-    if (winner) {
-        console.log(a, b, c);
-        status = `Winner: ${winner}`;
-    } else {
-        status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+        nextSquares[i] = 'X';
+        setSquares(nextSquares);
     }
 
     return (
         <>
-            <div className="status">{status}</div>
-            <div className="board">
-                <div className="board-row">
+            <div>
+                <div className="flex">
                     <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
                     <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
                     <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
                 </div>
-                <div className="board-row">
+                <div className="flex">
                     <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
                     <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
                     <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
                 </div>
-                <div className="board-row">
+                <div className="flex">
                     <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
                     <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
                     <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
@@ -48,4 +39,3 @@ export default function Board({ xIsNext, squares, onPlay }) {
         </>
     )
 }
-
