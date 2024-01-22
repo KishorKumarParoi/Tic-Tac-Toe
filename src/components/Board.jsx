2a5725beb669ef7f6774ea856ece3e0787e8e6f9
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2024-01-23 02:29:19 +0600

    Calculating winner is implemented with simple js function

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
 function Square({ value, onSquareClick }) {
-
     return (
         <>
             <button className="border border-5 border-gray-500  bg-white text-Black text-7xl h-32 w-32 font-semibold leading-9 m-2" onClick={onSquareClick}>{value}</button>
@@ -9,17 +8,53 @@ function Square({ value, onSquareClick }) {
     )
 }
 
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2], // top row
+        [3, 4, 5], // middle row
+        [6, 7, 8], // bottom row
+        [0, 3, 6], // left column
+        [1, 4, 7], // middle column
+        [2, 5, 8], // right column
+        [0, 4, 8], // left diagonal
+        [2, 4, 6], // right diagonal
+    ];
+    for (let line of lines) {
+        const [a, b, c] = line;
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
+            return { winner: squares[a], line: line };
+    }
+    return null;
+}
+
 export default function Board() {
     const [squares, setSquares] = useState(Array(9).fill(null));
+    const [xIsNext, setXIsNext] = useState(true);
+
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+        status = `Winner: ${winner.winner}`;
+    } else {
+        status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+    }
+
     function handleClick(i) {
+        if (squares[i] || calculateWinner(squares)) return;
         const nextSquares = squares.slice();
-        nextSquares[i] = 'X';
+        if (xIsNext) {
+            nextSquares[i] = 'X';
+        } else {
+            nextSquares[i] = 'O';
+        }
         setSquares(nextSquares);
+        setXIsNext(!xIsNext);
     }
 
     return (
         <>
             <div>
+                <h1 className="bg-yellow-500 text-black font-semibold pl-2">{status}</h1>
                 <div className="flex">
                     <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
                     <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
